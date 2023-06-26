e1f336c3679f0f0254d633d858b7c1d5ca0583b0
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-06-26 19:42:16 +0200

    created single card component with his elements and added it to the main app

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,15 +1,20 @@
 <script>
 import FirstSection from './mainparts/FirstSection.vue';
 import PromoBanner from './mainparts/PromoBanner.vue';
+import SingleCard from './mainparts/SingleCard.vue';
     export default {
         name: 'AppMain',
         components: {
             FirstSection,
             PromoBanner,
+            SingleCard,
         },
         data() {
             return {
-                
+                arrayCard: [{name: 'browse', section: ['Bed (1)', 'Food (6)', 'Toys (6)', 'Transport (7)'], img: ['product-9.jpg', 'product-3.jpg', 'product-2.jpg', 'product-4.jpg']}, 
+                            {name: 'bestsellers', section: ['Transport cage', 'Dog leash', 'Animal transport cage', 'Colorful cat leash'], price: ['$25.00', '$25.00', '<span class="text-decoration-line-through">$35.00</span> $25.00', '$12.00']},
+                            {name: 'articles', section: ['How to ‘loose leash walk’ your dog', 'Stop cats from scratching furniture', 'Where to buy the best pet food', 'Proper care for your pet toys'], date: ['April 21st, 2020', 'April 19st, 2020', 'April 18st, 2020', 'April 17st, 2020']}
+                    ],
             }
         },
         methods: {
@@ -25,9 +30,14 @@ import PromoBanner from './mainparts/PromoBanner.vue';
         <!-- ---Promo banner--- -->
         <PromoBanner/>
         <!-- ---Browse by category--- -->
+        <div class="browse d-flex flex-column">
+            <h1 class="m-auto">Browse by category</h1>
+            <p class="m-auto text-center">Augue purus et, tincidunt condimentum mauris. At nibh rutrum mi in. Nisi, vitae interdum eleifend dui, consequat nulla rhoncus dictum. Viverra.</p>
+            <SingleCard :card="arrayCard[0]"/>
+        </div>
         <!-- ---Food type card--- -->
         <!-- ---New arrivals weekly--- -->
-        <!-- ---Items everyone loves--- -->
+        <!-- ---All-time best sellers--- -->
         <!-- ---Users testimonials--- -->
         <!-- ---Newsletter--- -->
         <!-- ---Tips and tricks--- -->
@@ -40,4 +50,10 @@ import PromoBanner from './mainparts/PromoBanner.vue';
     @use '../styles/partials/mixins.scss' as *;
     @use '../styles/partials/variables.scss' as *;
 
+    .browse{
+        margin-top: 100px;
+        p{
+            width: 30%;
+        }
+    }
 </style>
---
+++ src/components/mainparts/SingleCard.vue
@@ -0,0 +1,48 @@
+<script>
+    export default {
+        name: 'SingleCard',
+        props: {
+            card: Object,
+        },
+        data() {
+            return {
+                
+            }
+        },
+        methods: {
+            getCardImg(img){
+                return new URL(`../../assets/img/${img}`, import.meta.url).href;
+            }
+        },
+    }
+</script>
+
+<template>
+    <div class="singleCardBrowse d-flex" v-if="card.name === 'browse'">
+        <div class="my-card" v-for="n in (card.section.length)">
+            <img :src='getCardImg(card.img[n-1])' alt="Product Image">
+            <p class="m-0">{{card.section[n-1]}}</p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    @use '../../styles/partials/mixins.scss' as *;
+    @use '../../styles/partials/variables.scss' as *;
+
+    .singleCardBrowse{
+        text-align: center;
+        font-size: 25px;
+        width: 65%;
+        margin: auto;
+        img{
+            width: 295px;
+        }
+        .my-card{
+            margin: 48px 12px;
+        }
+        .my-card:hover p{
+            color: $textGreen;
+        }
+    }
+</style>
