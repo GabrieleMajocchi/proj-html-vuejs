4d0c83247ab59c73dfd4e372e041dd45584c948c
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-06-26 23:26:19 +0200

    added single food type components with his elements and added it to the main app

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,19 +2,26 @@
 import FirstSection from './mainparts/FirstSection.vue';
 import PromoBanner from './mainparts/PromoBanner.vue';
 import SingleCard from './mainparts/SingleCard.vue';
+import SingleFoodtype from './mainparts/SingleFoodtype.vue';
     export default {
         name: 'AppMain',
         components: {
             FirstSection,
             PromoBanner,
             SingleCard,
+            SingleFoodtype,
         },
         data() {
             return {
                 arrayCard: [{name: 'browse', section: ['Bed (1)', 'Food (6)', 'Toys (6)', 'Transport (7)'], img: ['product-9.jpg', 'product-3.jpg', 'product-2.jpg', 'product-4.jpg']}, 
                             {name: 'bestsellers', section: ['Transport cage', 'Dog leash', 'Animal transport cage', 'Colorful cat leash'], price: ['$25.00', '$25.00', '<span class="text-decoration-line-through">$35.00</span> $25.00', '$12.00']},
                             {name: 'articles', section: ['How to ‘loose leash walk’ your dog', 'Stop cats from scratching furniture', 'Where to buy the best pet food', 'Proper care for your pet toys'], date: ['April 21st, 2020', 'April 19st, 2020', 'April 18st, 2020', 'April 17st, 2020']}
-                    ],
+                        ],
+                
+                foodlist: [{name: 'Kibble', type: 'Dry dog food', img: 'food-transparent-18.png', section: 'dry food'},
+                            {name: 'Moist', type: 'Canned dog food', img: 'food-transparent-17.png', section: 'moist food'},
+                            {name: 'Frozen', type: 'Freeze-Dried dog food', img: 'food-transparent-16.png', section: 'frozen food'}
+                        ],
             }
         },
         methods: {
@@ -36,6 +43,7 @@ import SingleCard from './mainparts/SingleCard.vue';
             <SingleCard :card="arrayCard[0]"/>
         </div>
         <!-- ---Food type card--- -->
+        <SingleFoodtype :foodlist="foodlist"/>
         <!-- ---New arrivals weekly--- -->
         <!-- ---All-time best sellers--- -->
         <!-- ---Users testimonials--- -->
---
+++ src/components/mainparts/SingleFoodtype.vue
@@ -0,0 +1,66 @@
+<script>
+    export default {
+        name: 'SingleFoodtype',
+        props: {
+            foodlist: Object,
+        },
+        data() {
+            return {
+                
+            }
+        },
+        methods: {
+            getCardImg(img){
+                return new URL(`../../assets/img/${img}`, import.meta.url).href;
+            }
+        },
+    }
+</script>
+
+<template>
+    <div class="foodtypeCards d-flex m-auto">
+        <div class="my-card d-flex flex-column align-items-center" v-for="foodtype in foodlist">
+            <h1>{{foodtype.name}}</h1>
+            <p>{{foodtype.type}}</p>
+            <img :src="getCardImg(foodtype.img)" :alt="foodtype.type">
+            <a href="#">Shop {{foodtype.section}}</a>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    @use '../../styles/partials/mixins.scss' as *;
+    @use '../../styles/partials/variables.scss' as *;
+
+    .foodtypeCards{
+        width: 65%;
+        .my-card{
+            background-image: url('../../assets/img/bg-transparent-3.png');
+            background-color: $bgDarkGreen;
+            width: 100%;
+            transition: all 0.3s;
+            margin: 0 8px;
+            &:hover{
+                transition: all 0.3s;
+                transform: scale(1.03);
+                filter: brightness(1.3);
+            }
+            h1{
+                color: $textColor;
+                padding-top: 75px;
+            }
+            p{
+                color: $textGreen;
+            }
+            img{
+                width: 100%;
+                padding: 30px 5rem 0;
+            }
+            a{
+                margin-bottom: 50px;
+                @include buttonOnlyGreen(0)
+                
+            }
+        }
+    }
+</style>
