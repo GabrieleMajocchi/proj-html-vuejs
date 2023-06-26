93fbdea24607bbd4bcdf68890ca0c6a57eba385c
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-06-26 11:55:58 +0200

    Created base structure with vue, vite, bootstrap, popper, fontawesome, sass

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+  import AppHeader from './components/AppHeader.vue';
+  import AppMain from './components/AppMain.vue';
+  import AppFooter from './components/AppFooter.vue';
+  
+  export default {
+    name: 'AppVue',
+    components:{
+        AppHeader,
+        AppMain,
+        AppFooter
+      }
+  }
+</script>
+
+<template>
+  <div>
+    <AppHeader/>
+    <AppMain/>
+    <AppFooter/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @import "bootstrap/scss/bootstrap";
+  @import './styles/general.scss';
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+
+import App from './App.vue'
+
+import * as bootstrap from 'bootstrap'
+
+createApp(App).mount('#app')
