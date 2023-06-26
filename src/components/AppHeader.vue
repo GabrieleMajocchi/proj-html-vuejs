6042f1c733ae419920be11811ca7457f2d180617
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-06-26 12:21:41 +0200

    added components to the header and array to make the nav

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,21 @@
 <script>
+import TopHeader from './headerparts/TopHeader.vue'
+import LowerHeader from './headerparts/LowerHeader.vue'
     export default {
         name: 'AppHeader',
+        components: {
+            TopHeader,
+            LowerHeader,
+        },
         data() {
             return {
-                
+                navSections: [{name: 'Home', element1: 'Home - Alternate'},
+                                {name: 'Shop', element1: 'Shop no sidebar', element2: 'Shop single sidebar', element3: 'Shop dual sidebar', element4: 'Simple product', element5: 'variable product'},
+                                {name: 'About'},
+                                {name: 'Blog'},
+                                {name: 'Contact'},
+                                {name: 'Shop by brand'}
+                    ],
             }
         },
         methods: {
@@ -13,9 +25,12 @@
 </script>
 
 <template>
-    <footer>
-        <h1>App Header</h1>
-    </footer>
+    <header>
+        <!-- ---Top header with Logo, search and user icon--- -->
+        <TopHeader/>
+        <!-- ---Lower header with navbar--- -->
+        <LowerHeader :navSections="navSections"/>
+    </header>
 </template>
 
 <style lang="scss" scoped>
